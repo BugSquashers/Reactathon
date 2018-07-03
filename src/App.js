1c442a5721ac63a1f2ea4bbe4d810a4a03f57eb2
BugSquashers <rupesh.deo@verizon.com>
2018-07-03 09:23:53 +0000

    logo change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 import OpenJobs from './job-portal-components/open-jobs/OpenJobs';
 import CandidateDetails from './job-portal-components/candidate-details/CandidateDetails';
@@ -9,12 +9,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} />
+         <h1 className="App-title">Squashers Job Portal</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       
+         
 		<OpenJobs/>
 		<CandidateDetails/>
       </div>
