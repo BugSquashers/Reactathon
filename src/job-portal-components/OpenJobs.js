21b839de1b3e4affd92c6d0140d0d2e879f63a36
BugSquashers <rupesh.deo@verizon.com>
2018-07-03 08:33:52 +0000

    component package

---
+++ src/job-portal-components/OpenJobs.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+class OpenJobs extends Component {
+  render() {
+    return (
+      <div >
+       
+        <h2>Open Jobs page</h2>
+		
+		
+      </div>
+    );
+  }
+}
+
+export default OpenJobs;
