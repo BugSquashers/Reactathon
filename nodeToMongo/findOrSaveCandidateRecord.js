87ad4c012249f4016cd0b9e07e1abe63e19bda12
rupesh <rupesh.deo@verizon.com>
2018-07-03 14:58:53 +0000

    Merge all services in one file.

--- nodeToMongo/findOrSaveCandidateRecord.js
+++ nodeToMongo/findOrSaveCandidateRecord.js
@@ -54,5 +54,59 @@ res.send(doc);
 });
 
 });
+
+
+ 
+// create a client to mongodb
+var MongoClient = require('mongodb').MongoClient;
+ 
+
+
+app.get('/findJobDetails', function (req, res) {
+   
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    // db pointing to newdb
+     var dbo=db.db("jobsDB");
+var query = { jobId : req.param("jobId")};
+  dbo.collection("jobDetails").find(query).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+res.end( JSON.stringify(result));
+    db.close();
+  });
+console.log("Switched to "+dbo.databaseName+" database");
+    // create 'users' collection in newdb database
+// insert document to 'users' collection using insertOne
+   
+});
+
+      
+});
+
+
+app.get('/listJobDetails', function (req, res) {
+   
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    // db pointing to newdb
+     var dbo=db.db("jobsDB");
+
+  dbo.collection("jobDetails").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+res.end( JSON.stringify(result));
+    db.close();
+  });
+console.log("Switched to "+dbo.databaseName+" database");
+    // create 'users' collection in newdb database
+// insert document to 'users' collection using insertOne
+   
+});
+
+      
+});
 app.listen(8081);
 console.log('Running');
