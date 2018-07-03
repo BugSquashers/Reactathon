1d9ef656ecf15bd89db19c19403c0a37e0eaaead
BugSquashers <rupesh.deo@verizon.com>
2018-07-03 10:59:56 +0000

    service call

--- src/job-portal-components/open-jobs/OpenJobs.js
+++ src/job-portal-components/open-jobs/OpenJobs.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './OpenJobs.css';
 
 class OpenJobs extends Component {
+	
+  state = {
+    response: ''
+  };
+
+  serviceUrl: 'http://abc.com/abc'
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch(this.serviceUrl);
+    const body = await response.json();
+	
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+	
+	
   render() {
+	 
     return (
       <div >
        
         <h2>Open Jobs page</h2>
-		
+		<p>{this.state.response}</p>
 		
       </div>
     );
