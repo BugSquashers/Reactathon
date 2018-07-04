126a44cc35f96b9982d8157465252e263bdcbca7
rupesh <rupesh.deo@verizon.com>
2018-07-04 07:01:07 +0000

    Working version of search job.

--- src/job-portal-components/search-jobs/SearchJobs.js
+++ src/job-portal-components/search-jobs/SearchJobs.js
@@ -5,9 +5,14 @@ class SearchJobs extends Component {
 	
 	
 	state = {
+		response: [],
     skillSet: ''
+	
   };
   
+  
+
+
 	searchJobs = () => {
    console.log('Called');
 	this.callApi()
@@ -16,21 +21,31 @@ class SearchJobs extends Component {
   }
   
   callApi = async () => {
-	  
-   console.log('Called1 -->' + this.state.skillSet);
-	  fetch('/searchJobsDetails', {
+	  let searchJobRequest={ coreSkils: this.state.skillSet
+
+}
+	 const response = await fetch('/searchJobsDetails', {
         method: 'POST',
-        data: {
-          coreSkils: 'micro'
-        }
-      })
-      .then(function(response) {
-		  console.log(response);
-        return response.json()
-      }).then(function(body) {
-        console.log(body);
+		headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+        body: JSON.stringify(searchJobRequest)
       });
+	  
+	  const body = await response.json();
+	
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+	  
+	  
+	  
   };
+  
+  
+
+
   
   handleSkillSetChange = (e) =>{
    this.setState({skillSet: e.target.value});
@@ -41,6 +56,17 @@ applyForJob = (jobId) => {
   }
 
   render() {
+	  const response = this.state.response.map((item, i) => (
+      <tr>
+        <td>{ item.jobId }</td>
+		<td>{ item.coreSkils }</td>
+		<td>{ item.position }</td>
+		<td>{ item.location }</td>
+		<td>{ item.noOfOpenings }</td>
+		<td><input type="button" value="Apply" onClick={(e)=>this.applyForJob(item.jobId, e)}/></td>
+      </tr>
+    ));
+	
     return (
 	
 	<div id="search">
@@ -57,6 +83,27 @@ applyForJob = (jobId) => {
 								</p>
                                 
                             </form>
+							
+							
+							
+	
+	
+   <table border="1">
+		<thead>
+			<tr>
+				
+				<th>Job Id</th>
+				<th> Core Skills</th>
+				<th> Position</th>
+				<th> Location</th>
+				<th>No of Openings</th>
+				<th>Apply</th>
+			</tr>
+		</thead>
+		<tbody>{ response }</tbody>
+	  </table>
+  
+  
                         </div>);
   }
 }
